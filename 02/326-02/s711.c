7ed25fc4b633c7aed756d05508585f88193846d3
Josiah <josiahnelson1@gmail.com>
2017-03-19 10:57:56 +1300

    compress 711 for submission

--- 02/326-02/s711.c
+++ 02/326-02/s711.c
@@ -1,33 +1,29 @@
+/**
+ * s711.c
+ * COSC326 Etude 2
+ * 711 Problem
+ *
+ * @author Rhianne Price
+ * March 2017
+ **/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 /**
- * Memory allcation with error checking.
- */
-void *emalloc(size_t s){
-  void *result = malloc(s);
-  if(result == NULL){
-    fprintf(stderr, "Memory allocation failed!");
-    exit(EXIT_FAILURE);
-  }
-  return result;
-}
-
-/**
  * Finds four prices that sum and multiply to $7.11.
  *
- * Finds all divisors to 4 sf of 7.11, checks all 3 number combinations
- * of those divisors for combos that sum to less than 7.11, then checks if
- * those 3 and the difference between the sum of the 3 and 7.11 multiply to
+ * Finds all divisors to 2 dp of 7.11, checks all combinations of 3 numbers
+ * for combos that sum to less than 7.11, then checks if those 3 and the
+ * difference between the sum of the 3 and 7.11 multiply to
  * and prints it if it does.
  */
 int main(){
     double a, b, c, d;
-    int i, j, k, divisor_count;
+    int i, j, k;
     int TOTAL = 711;
-    double *divisors = emalloc(TOTAL * sizeof divisors);
-    divisor_count = 0;
+    double *divisors = malloc(TOTAL * sizeof divisors);
+    int divisor_count = 0;
 
     /* Find all clean divisors of 7.11. */
     for (i = 1; i <= TOTAL; i++) {
