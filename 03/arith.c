3c33f1c28b5b027c15d50cafba14b69f4d932f2e
Josiah <josiahnelson1@gmail.com>
2017-03-25 14:03:58 +1300

    arithmetic scanner

--- 03/arith.c
+++ 03/arith.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void *emalloc(size_t s){
-  void *result = malloc(s);
-
-  if(NULL == result){
-    fprintf(stderr, "Memory allocation failed!\n");
-    exit(EXIT_FAILURE);
-  }
-  return result;
-}
-
 int main() {
+  int size = 1;
+  int *numbers = malloc(size * sizeof numbers);
+  int number;
+  char *line;
+  int i;
 
-  while(1 == scanf("%s\n", );)
+  while(2 == scanf("%s[^\n] %s[^\n]", );)
+
+  while(1 == scanf("%d", &number)) {
+    numbers[size - 1] = number;
+    size++;
+    numbers = realloc(numbers, size * sizeof numbers);
+  }
+
+  for(i = 0; i < size - 1; i++) {
+    printf("%d\n", numbers[i]);
+  }
+
+  free(numbers);
+
   return EXIT_SUCCESS;
 }
