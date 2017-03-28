e8188e009f66c7afea8934e05b3f65566a84f423
Rhianne Price <price.rhianne@gmail.com>
2017-03-28 22:51:27 +1300

    suss arithmetic

--- 03/arith.c
+++ 03/arith.c
@@ -1,26 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define PLUS 0
+#define TIMES 1
 
 int main() {
-  int size = 1;
-  int *numbers = malloc(size * sizeof numbers);
-  int number;
-  char *line;
-  int i;
+  int *numbers = malloc(25 * sizeof numbers);
+  int number, total, i, bits, size; sum;
+  char order = '\0';
+  int *operators;
 
-  while(2 == scanf("%s[^\n] %s[^\n]", );)
+  while(!feof(stdin)){
+    i = 0;
+    while(1 == scanf("%d", &number)){
+      numbers[i++] = number;
+    }
+    total = numbers[size = i - 1];
+    scanf("%c\n", &order);
 
-  while(1 == scanf("%d", &number)) {
-    numbers[size - 1] = number;
-    size++;
-    numbers = realloc(numbers, size * sizeof numbers);
-  }
-
-  for(i = 0; i < size - 1; i++) {
-    printf("%d\n", numbers[i]);
+    operators = malloc((size - 1) * sizeof operators);
+    for(i = 0; i < pow(2, size - 1); i++) {
+      for(bits = 0; bits < size - 1; bits++) {
+        operators[bits] = (i >> bits) & 1;
+      }
+      if(order == 'L') {
+        solveL(numbers, size, operators, total);
+      } else if(order == 'N') {
+        solveN(numbers, size, operators, total);
+      }
+    }
+    free(operators);
   }
 
   free(numbers);
 
   return EXIT_SUCCESS;
+}
+
+char *solveN(int *numbers, int size, int *operators, int total) {
+
+}
+
+void solveL(int *numbers, int size, int *operators, int total) {
+  int i, j;
+  int sum = numbers[0];
+
+  for(i = 0; i < size - 1; i++) {
+    if(operators[i] == PLUS) {
+      sum += numbers[i + 1];
+    } else if(operators[i] == TIMES){
+      sum *= numbers[i + 1];
+    }
+    if(sum > target) break;
+    if(sum == target) {
+      for(j = 0; j < i; j++) {
+        printf("%d %c ", numbers[j], operators[j] == PLUS ? '+' : '*');
+      }
+    }
+  }
+  printf("no solution\n");
 }
