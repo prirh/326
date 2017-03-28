3343b10f56762bba403f47e37f6bbdc3688c336a
Rhianne Price <price.rhianne@gmail.com>
2017-03-29 12:53:22 +1300

    arithmetic for left to right working

--- 03/arith.c
+++ 03/arith.c
@@ -1,19 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
 
 #define PLUS 0
 #define TIMES 1
 
+int solve(int *numbers, int size, int *operators, int total, char order) {
+  int i, j;
+  int first = numbers[0];
+  int sum;
+  int* working = malloc(size * sizeof working);
+
+  if(size == 1 & numbers[0] == total){
+    printf("%c %d\n", order, total);
+    return 1;
+  }
+  /* Left to right. */
+  if(order == 'L'){
+    sum = first;
+    for(i = 0; i < size - 1; i++) {
+      if(operators[i] == PLUS) {
+        sum += numbers[i + 1];
+      } else if(operators[i] == TIMES){
+        sum *= numbers[i + 1];
+      }
+      if(sum > total) return 0;
+      if(i == size - 2 && sum == total) {
+        printf("%c %d", order, first);
+        for(j = 0; j <= i; j++){
+          printf(" %c %d", operators[j] == PLUS ? '+' : '*', numbers[j + 1]);
+        }
+        printf("\n");
+        return 1;
+      }
+    }
+  }
+
+  /* Normal order - multiplication first. */
+  if(order == 'N'){
+    for(i = 0; i < size - 1; i++) {
+      if(operators[i] == TIMES) {
+        working[i] = 0;
+        working[i + 1] = numbers[i + 1] * numbers[i];
+        if(working[i + 1] > total) return 0;
+      }
+    }
+    sum = working[0];
+    for(i = 0; i < size - 1; i++) {
+        sum += working[i + 1];
+    }
+    if(sum > total) return 0;
+    if(i == size - 2 && sum == total) {
+      printf("%c %d", order, first);
+      for(j = 0; j <= i; j++){
+        printf(" %c %d", operators[j] == PLUS ? '+' : '*', numbers[j + 1]);
+      }
+        printf("\n");
+        return 1;
+      }
+    }
+  free(working);
+  return 0;
+}
+
 int main() {
   int *numbers = malloc(25 * sizeof numbers);
-  int number, total, i, bits, size; sum;
+  int number, total, i, bits, size;
   char order = '\0';
   int *operators;
+  int solutions;
 
   while(!feof(stdin)){
     i = 0;
+    solutions = 0;
     while(1 == scanf("%d", &number)){
       numbers[i++] = number;
     }
@@ -25,40 +84,13 @@ int main() {
       for(bits = 0; bits < size - 1; bits++) {
         operators[bits] = (i >> bits) & 1;
       }
-      if(order == 'L') {
-        solveL(numbers, size, operators, total);
-      } else if(order == 'N') {
-        solveN(numbers, size, operators, total);
-      }
+      solutions += solve(numbers, size, operators, total, order);
+    }
+    if(solutions == 0) {
+      printf("%c impossible\n", order);
     }
     free(operators);
   }
-
   free(numbers);
-
   return EXIT_SUCCESS;
-}
-
-char *solveN(int *numbers, int size, int *operators, int total) {
-
-}
-
-void solveL(int *numbers, int size, int *operators, int total) {
-  int i, j;
-  int sum = numbers[0];
-
-  for(i = 0; i < size - 1; i++) {
-    if(operators[i] == PLUS) {
-      sum += numbers[i + 1];
-    } else if(operators[i] == TIMES){
-      sum *= numbers[i + 1];
-    }
-    if(sum > target) break;
-    if(sum == target) {
-      for(j = 0; j < i; j++) {
-        printf("%d %c ", numbers[j], operators[j] == PLUS ? '+' : '*');
-      }
-    }
-  }
-  printf("no solution\n");
 }
