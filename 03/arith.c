87841a61f784f9f40106088036d4c5a646eb75d2
Rhianne Price <price.rhianne@gmail.com>
2017-03-29 22:27:25 +1300

    add comments to arithmetic

--- 03/arith.c
+++ 03/arith.c
@@ -1,3 +1,11 @@
+/**
+ * arithmetic.c
+ * COSC326 Etude 3
+ * Arithmetic
+ *
+ * @author Rhianne Price
+ * March 2017
+ **/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -16,84 +24,89 @@ void *emalloc(size_t s){
   }
   return result;
 }
-
-int solve(int *numbers, int size, int *operators, int total, char order) {
+/**
+ * Given a set of numbers, operations, and a target value, this function prints
+ * an expression using all of those numbers and operations in the order given,
+ * that evaluatesto the target when read from left to right, and returns 1.
+ * If the expression does not evaluate to the target, it returns 0;
+ */
+int solve_left_to_right(int *numbers, int size, int *operators, int total) {
   int i, j;
   int first = numbers[0];
-  int sum;
-  int *working;
+  int sum = first;
 
-  if(size == 1 && numbers[0] == total){
-    printf("%c %d\n", order, total);
-    return 1;
-  }
-  /* Left to right. */
-  if(order == 'L'){
-    sum = first;
-    for(i = 0; i < size - 1; i++) {
-      if(operators[i] == PLUS) {
-        sum += numbers[i + 1];
-      } else if(operators[i] == TIMES){
-        sum *= numbers[i + 1];
-      }
-      if(sum > total) return 0;
-      if(i == size - 2 && sum == total) {
-        printf("%c %d", order, first);
-        for(j = 0; j <= i; j++){
-          printf(" %c %d", operators[j] == PLUS ? '+' : '*', numbers[j + 1]);
-        }
-        printf("\n");
-        return 1;
-      }
+  for(i = 0; i < size - 1; i++) {
+    if(operators[i] == PLUS) {
+      sum += numbers[i + 1];
+    } else if(operators[i] == TIMES){
+      sum *= numbers[i + 1];
     }
-  }
-
-  /* Normal order - multiplication first. */
-  if(order == 'N'){
-    working = emalloc(size * sizeof working);
-    working[0] = numbers[0];
-    for(i = 0; i < size - 1; i++) {
-      working[i + 1] = numbers[i + 1];
-      if(operators[i] == TIMES) {
-        working[i + 1] = numbers[i + 1] * numbers[i];
-        working[i] = 0;
-        if(working[i + 1] > total){
-          free(working);
-          return 0;
-        }
-      }
-    }
-    sum = working[0];
-
-    for(i = 1; i < size; i++) {
-        sum += working[i];
-    }
-    if(sum > total){
-      free(working);
-      return 0;
-    }
-    if(sum == total) {
-      printf("%c %d", order, first);
-      for(j = 0; j < size - 1; j++){
+    if(i == size - 2 && sum == total) {
+      printf("L %d", first);
+      for(j = 0; j <= i; j++){
         printf(" %c %d", operators[j] == PLUS ? '+' : '*', numbers[j + 1]);
       }
-        printf("\n");
-        free(working);
-        return 1;
-      }
+      printf("\n");
+      return 1;
     }
-  free(working);
+  }
   return 0;
 }
 
+/**
+ * Given a set of numbers, operations, and a target value, this function prints
+ * an expression using all of those numbers and operations in the order given,
+ * that evaluates to the target when multiplication preceeds addition,
+ * and returns 1. If the expression does not evaluate to the target,
+ * it returns 0;
+ */
+int solve_normally(int *numbers, int size, int *operators, int total) {
+  int i, j, sum;
+  int first = numbers[0];
+  int *working = emalloc(size * sizeof working);
+
+  working[0] = first;
+  for(i = 0; i < size - 1; i++) {
+    working[i + 1] = numbers[i + 1];
+    if(operators[i] == TIMES) {
+      working[i + 1] = numbers[i + 1] * numbers[i];
+      working[i] = 0;
+      if(working[i + 1] > total){
+        free(working);
+        return 0;
+      }
+    }
+  }
+  sum = working[0];
+
+  for(i = 1; i < size; i++) {
+      sum += working[i];
+  }
+  if(sum == total) {
+    printf("N %d", first);
+    for(j = 0; j < size - 1; j++){
+      printf(" %c %d", operators[j] == PLUS ? '+' : '*', numbers[j + 1]);
+    }
+      printf("\n");
+      free(working);
+      return 1;
+    }
+free(working);
+return 0;
+}
+
+/**
+ * Scans in numbers to be used in the expression, target value, and order of
+ * operations from stdin. For each scenario, all the possible combinations of
+ * + and * are passed to either of the two solver methods to check for and print
+ * solutions.
+ */
 int main() {
   int maxSize = 25;
   int *numbers = emalloc(25 * sizeof numbers);
-  int number, total, bits, size, possible_combos;
-  int i;
+  int number, total, bit, size, possible_combos, i, solutions;
   char order = '\0';
   int *operators;
-  int solutions;
 
   while(!feof(stdin)){
     i = 0;
@@ -108,13 +121,22 @@ int main() {
     total = numbers[size = i - 1];
     scanf("%c\n", &order);
 
+    if(size == 1 && numbers[0] == total){
+      printf("%c %d\n", order, total);
+      continue;
+    }
+
     possible_combos = pow(2, size - 1);
     operators = emalloc((size - 1) * sizeof operators);
     for(i = 0; i < possible_combos; i++) {
-      for(bits = 0; bits < size - 1; bits++) {
-        operators[bits] = (i >> bits) & 1;
+      for(bit = 0; bit < size - 1; bit++) {
+        operators[bit] = (i >> bit) & 1;
       }
-      solutions += solve(numbers, size, operators, total, order);
+      if(order == 'N') {
+        solutions += solve_normally(numbers, size, operators, total);
+      } else if (order == 'L'){
+        solutions += solve_left_to_right(numbers, size, operators, total);
+      }
     }
     if(solutions == 0) {
       printf("%c impossible\n", order);
