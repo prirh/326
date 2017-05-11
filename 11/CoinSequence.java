e1ad9cd09adc7a831c091d81251a988846302114
Josiah <josiahnelson1@gmail.com>
2017-05-11 19:24:51 +1200

    search tree working perfectly for up to 12 coins

--- 11/CoinSequence.java
+++ 11/CoinSequence.java
@@ -5,11 +5,24 @@ public class CoinSequence {
     String sequence;
     List<CoinSequence> children;
     int depth;
+    int gaps;
+    int runs;
+    CoinSequence parent;
 
-    public CoinSequence(String sequence, int depth) {
+    public CoinSequence(String sequence, int depth, int gaps, CoinSequence parent) {
         this.depth = depth;
         this.sequence = sequence;
-        children = new ArrayList<CoinSequence>();
+        this.gaps = gaps;
+        this.children = new ArrayList<CoinSequence>();
+        this.parent = parent;
+
+        // for(int i = 0;  i < sequence.length() - 1; i++) {
+        //     if(sequence.charAt(i) == '.' && sequence.charAt(i + 1) == '.') {
+        //         this.gaps++;
+        //     } else if(sequence.charAt(i) == '-' && sequence.charAt(i + 1) == '-') {
+        //         this.gaps++;
+        //     }
+        // }
     }
 
     public int checkChildren(String target) {
@@ -17,7 +30,7 @@ public class CoinSequence {
         return 0;
     }
 
-    public int addChildren(String target) {
+    public CoinSequence addChildren(String target) {
         ArrayList<Integer> pairs = new ArrayList<Integer>();
         ArrayList<Integer> gaps = new ArrayList<Integer>();
         for(int i = 0;  i < sequence.length() - 1; i++) {
@@ -34,6 +47,11 @@ public class CoinSequence {
             for(Integer gapIndex : gaps) {
                 String childSequence = "";
                 char[] s = sequence.toCharArray();
+                int childGaps = this.gaps;
+
+                String end = "..";
+                String start = "..";
+
 
                 /* Fill gap with pair. */
                 s[gapIndex] = sequence.charAt(pairIndex);
@@ -41,25 +59,41 @@ public class CoinSequence {
 
                 s[pairIndex] = '-';
                 s[pairIndex + 1] = '-';
+                if(gapIndex == 0) {
+                    childSequence += start;
+                    childGaps++;
+                }
 
-                if(pairIndex == 2 || pairIndex == sequence.length() - 4) {
-                    s[gapIndex] = '.';
-                    s[gapIndex + 1] = '.';
-                    childSequence += new String(s);
+                if(pairIndex == 2 && gapIndex != 0) {
+                    s[pairIndex] = '.';
+                    s[pairIndex + 1] = '.';
+                    childSequence += new String(s, 2, sequence.length() - 2);
+                    start = "";
+                } else if(pairIndex == sequence.length() - 4 && gapIndex != sequence.length() - 2) {
+                    s[pairIndex] = '.';
+                    s[pairIndex + 1] = '.';
+                    childSequence += new String(s, 0, sequence.length() - 2);
+                    end = "";
                 } else {
-                    if(gapIndex == 0) {
-                        childSequence += "..";
-                    }
                     childSequence += new String(s);
+                }
 
-                    if(gapIndex == sequence.length() - 2) {
-                        childSequence += "..";
+                if(gapIndex == sequence.length() - 2) {
+                    childSequence += end;
+                    childGaps++;
+                }
+
+                CoinSequence child = new CoinSequence(childSequence, depth + 1, childGaps, this);
+                if(childSequence.equals(target)) return child;
+                if(childGaps <= 3) {
+                    if(!children.isEmpty() && child.gaps < children.get(0).gaps) {
+                        children.add(0, child);
+                    } else {
+                        children.add(child);
                     }
                 }
-                if(new String(s).equals(target)) return depth + 1;
-                children.add(new CoinSequence(childSequence, depth + 1));
             }
         }
-        return 0;
+        return null;
     }
 }
--- 11/CoinTree.java
+++ 11/CoinTree.java
@@ -1,94 +1,116 @@
 package etude11;
 /**
- * CoinTree.java
- * COSC326 Etude 11
- * Heads and Tails
- *
- * @author Erina Jeffery
- * @author Rhianne Price
- * May 2017
- **/
+* CoinTree.java
+* COSC326 Etude 11
+* Heads and Tails
+*
+* @author Erina Jeffery
+* @author Rhianne Price
+* May 2017
+**/
 import java.util.*;
 
 public class CoinTree {
-      private String coins;
-      private String target;
-      private int heads;
-      private int tails;
-      private char first;
-      private CoinSequence root;
+  private String coins;
+  private String target;
+  private int heads;
+  private int tails;
+  private char first;
+  private CoinSequence root;
 
-      public CoinTree(int heads, int tails) {
-        this.heads = heads;
-        this.tails = tails;
-        first = tails < heads ? 'H' : 'T';
-        char other = first == 'H' ? 'T' : 'H';
-        target = "";
-        for(int i = 0; i < heads + tails; i++) {
-          if(i % 2 == 0) target += first;
-          else target += other;
-        }
-
-        coins = "..";
-        while(heads > 0) {
-          coins += "H";
-          heads--;
-        }
-        while(tails > 0) {
-          coins += "T";
-          tails--;
-        }
-        coins += "..";
-        root = new CoinSequence(coins, 0);
-
-      }
-        /**
-         * Reads the number of heads and tails from the command line, and prints out
-         * the number of steps to alternate reach the final solution.
-         * @param args first argument specifies the number of heads, second for tails.
-         *             if given a third argument s, the steps of the solution will
-                       we printed.
-         */
-        public static void main(String[] args) {
-          int heads = Integer.parseInt(args[0]);
-          int tails = Integer.parseInt(args[1]);
-          boolean show = false;
-          if(args.length == 3) show = "s".equals(args[2]);
-          int steps = 0;
-
-          if(Math.abs(heads - tails) > 1) {
-            System.out.println("Impossible");
-            return;
-          }
-
-          if(heads == 1 && tails == 1) {
-            System.out.println("0 moves");
-            return;
-          }
-
-          CoinTree coins = new CoinTree(heads, tails);
-
-        //   System.out.println(coins.target);
-          System.out.println(coins.root.sequence);
-          coins.root.addChildren(coins.target);
-          for(CoinSequence child : coins.root.children) {
-              System.out.println(child.sequence);
-          }
-
-        //   Queue<CoinSequence> searchQ = new ArrayDeque<CoinSequence>();
-        //   searchQ.add(coins.root);
-        //   while(!searchQ.isEmpty()) {
-        //       CoinSequence currentSequence = searchQ.poll();
-        //       if(currentSequence.addChildren(coins.target) > 0) {
-        //           System.out.println(currentSequence.depth + 1);
-        //           return;
-        //       }
-        //       for(CoinSequence child : currentSequence.children) {
-        //            searchQ.add(child);
-        //            System.out.println(child.sequence + " " + child.depth);
-        //       }
-        //   }
-        // }
+  public CoinTree(int heads, int tails) {
+    this.heads = heads;
+    this.tails = tails;
+    first = tails < heads ? 'H' : 'T';
+    char other = first == 'H' ? 'T' : 'H';
+    target = "..";
+    for(int i = 0; i < heads + tails; i++) {
+      if(i % 2 == 0) target += first;
+      else target += other;
     }
 
+    target += "..";
+
+    coins = "..";
+    while(heads > 0) {
+      coins += "H";
+      heads--;
+    }
+    while(tails > 0) {
+      coins += "T";
+      tails--;
+    }
+    coins += "..";
+    root = new CoinSequence(coins, 0, 2, null);
+
+  }
+  /**
+  * Reads the number of heads and tails from the command line, and prints out
+  * the number of steps to alternate reach the final solution.
+  * @param args first argument specifies the number of heads, second for tails.
+  *             if given a third argument s, the steps of the solution will
+  we printed.
+  */
+  public static void main(String[] args) {
+    int heads = Integer.parseInt(args[0]);
+    int tails = Integer.parseInt(args[1]);
+    boolean show = false;
+    if(args.length == 3) show = "s".equals(args[2]);
+    int steps = 0;
+
+    if(Math.abs(heads - tails) > 1) {
+      System.out.println("Impossible");
+      return;
+    }
+
+    if(heads == 1 && tails == 1) {
+      System.out.println("0 moves");
+      return;
+    }
+
+    CoinTree coins = new CoinTree(heads, tails);
+
+    //   System.out.println(coins.target);
+    // System.out.println(coins.root.sequence);
+    // coins.root.addChildren(coins.target);
+    //   for(CoinSequence child : coins.root.children) {
+    //       System.out.println(child.sequence);
+    //   }
+
+    // Queue<CoinSequence> searchQ = new PriorityQueue<CoinSequence>(heads + tails, new Comparator<CoinSequence>(){
+    //   @Override
+    //   public int compare(CoinSequence c1, CoinSequence c2) {
+    //     if(c1.gaps > c2.gaps) return 1;
+    //     if(c1.gaps < c2.gaps) return -1;
+    //     else return 0;
+    //   }
+    // });
+
+    CoinSequence last = null;
+    ArrayList<CoinSequence> solution = new ArrayList<CoinSequence>();
+
+    Queue<CoinSequence> searchQ = new ArrayDeque<CoinSequence>();
+    searchQ.add(coins.root);
+    while(!searchQ.isEmpty()) {
+      CoinSequence currentSequence = searchQ.poll();
+      last = currentSequence.addChildren(coins.target);
+      if(last != null) break;
+      for(CoinSequence child : currentSequence.children) {
+        searchQ.add(child);
+        // System.out.println(child.sequence + " " + child.depth);
+      }
+    }
+    System.out.println(last.depth);
+    if(show) {
+      CoinSequence current = last;
+      while(current.parent != null) {
+        solution.add(0, current);
+        current = current.parent;
+      }
+      solution.add(0, coins.root);
+      for(CoinSequence step : solution) {
+        System.out.println(step.sequence);
+      }
+    }
+  }
 }
